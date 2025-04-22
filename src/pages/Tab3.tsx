8e4a7cc0379c5ace1b9a3cc9aff05ea26f9c9488
junAD89 <balhighlight@gmail.com>
2025-04-22 13:44:07 +0000

    test app flow

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,12 +1,42 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
+import * as LiveUpdates from '@capacitor/live-updates';
 
 const Tab3: React.FC = () => {
+
+
+
+
+
+
+  const checkUpdate = async () => {
+    try {
+      const result = await LiveUpdates.sync();
+      console.log('Live update result:', result);
+      alert('Update status: ' + (result as any).status);
+
+    } catch (err) {
+      console.error('Live update error:', err);
+      alert('Erreur Live Update');
+    }
+  };
+
+
+
+
+
+
+
+
   return (
     <IonPage>
       <h1>
         Avant le Appflow
       </h1>
+
+      <button onClick={checkUpdate}>
+        Tester mise à jour
+      </button>
     </IonPage>
   );
 };
