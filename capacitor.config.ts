87eb6e552d9644e6d37573ccde4b3de12005b756
junAD89 <balhighlight@gmail.com>
2025-04-22 17:01:04 +0000

    refactor(capacitor.config.ts): simplify config by removing custom interface
    The custom interface `MyCapacitorConfig` was removed and the `liveUpdates` configuration was moved into the `plugins` section. This change aligns the configuration with the standard Capacitor setup and improves maintainability.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
-import { LiveUpdateConfig } from '@capacitor/live-updates';
 
-interface MyCapacitorConfig extends CapacitorConfig {
-  liveUpdates: LiveUpdateConfig;
-}
-
-const config: MyCapacitorConfig = {
+const config: CapacitorConfig = {
   appId: 'fast.learn.english',
   appName: 'learnAnglish',
   webDir: 'dist',
-  liveUpdates: {
-    appId: '654159aa',
-    channel: 'Production',
-    autoUpdateMethod: 'background',
-    maxVersions: 2
+  plugins: {
+    LiveUpdates: {  // Notez que c'est "LiveUpdates" et non "liveUpdates"
+      appId: '654159aa',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
   }
 };
 
