9b74f803bd458a99a9088d2bd95a71f25b052ed6
junAD89 <balhighlight@gmail.com>
2025-04-22 20:21:34 +0000

    ajout du modal dans le tab 1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -3,36 +3,40 @@ import './Tab1.css';
 import ButtonPrincipale from '../components/Button.tsx/ButtonPrincipale';
 import axios from 'axios';
 
+import Modal from "react-modal";
+import { useState } from 'react';
+
+
 const Tab1: React.FC = () => {
 
-  const handleServer = async () => {
-    try {
-      const response = await axios.post("http://localhost:3000/test", {
-        message: "Je suis beau",
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-      })
-
-      console.log(response.data);
-
-    } catch (error) {
-
-      alert(error);
-
-    }
+  function openModal() {
+    setIsOpen(true);
   }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <IonPage>
 
-      <IonButton
-        onClick={() => {
-          handleServer()
-        }}
-      >
-
-        handle server
+      <IonButton onClick={openModal}>
+        Ouvrir Modal
       </IonButton>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Exemple Modal"
+      >
+        <div>Contenu ici</div>
+        <button onClick={closeModal}>Fermer</button>
+      </Modal>
+
     </IonPage>
   );
-};
+}
 
 export default Tab1;
