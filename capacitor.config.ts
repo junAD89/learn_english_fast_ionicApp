17873707f725a8f02309fd355e2fb811dda6133c
junAD89 <balhighlight@gmail.com>
2025-04-25 19:50:06 +0000

    ajout de config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,23 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import { LiveUpdateConfig } from '@capacitor/live-updates';
 
-const config: CapacitorConfig = {
+// Étendre l'interface CapacitorConfig pour inclure liveUpdates
+interface MyCapacitorConfig extends CapacitorConfig {
+  liveUpdates: LiveUpdateConfig;
+}
+
+const config: MyCapacitorConfig = {
   appId: 'com.studiesapp.nom',
   appName: 'learnAnglish',
   webDir: 'dist',
+  // Déplacer LiveUpdates hors des plugins
+  liveUpdates: {
+    appId: 'com.studiesapp.nom',
+    channel: 'Production',
+    autoUpdateMethod: 'background',
+    maxVersions: 2
+  },
   plugins: {
-    LiveUpdates: {  // Notez que c'est "LiveUpdates" et non "liveUpdates"
-      appId: 'com.studiesapp.nom',
-      channel: 'Production',
-      autoUpdateMethod: 'background',
-      maxVersions: 2
-    },
     LocalNotifications: {
       smallIcon: 'ic_stat_myicon',
       iconColor: '#488AFF',
