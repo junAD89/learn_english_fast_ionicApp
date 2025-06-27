6bc677ef28cb084a95f73a618c7ce0b02dcfc398
junAD89 <balhighlight@gmail.com>
2025-06-27 18:28:44 +0000

    fixing

--- android/app/src/main/java/com/studiesapp/nom/MainActivity.java
+++ android/app/src/main/java/com/studiesapp/nom/MainActivity.java
@@ -1,16 +1,30 @@
 package com.studiesapp.nom;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.getcapacitor.BridgeActivity;
 import com.startapp.sdk.adsbase.StartAppSDK;
 import com.studiesapp.nom.StartAppPlugin;
 
 public class MainActivity extends BridgeActivity {
-    @Override 
-    public void onCreate(Bundle savedInstanceState){
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "=== BEFORE plugin registration ===");
+        
+        // ENREGISTRER LE PLUGIN EN PREMIER
+        registerPlugin(StartAppPlugin.class);
+        Log.d("MainActivity", "Plugin registered BEFORE super.onCreate");
+        
+        // PUIS appeler super.onCreate()
         super.onCreate(savedInstanceState);
+        Log.d("MainActivity", "super.onCreate() called");
+        
+        // ENFIN initialiser StartApp
         StartAppSDK.init(this, "205126138", false);
-        registerPlugin(StartAppPlugin.class); // ← Ça !
+        Log.d("MainActivity", "StartApp SDK initialized");
+        
+        Log.d("MainActivity", "=== MainActivity complete ===");
     }
 }
--- android/app/src/main/java/com/studiesapp/nom/StartAppPlugin.java
+++ android/app/src/main/java/com/studiesapp/nom/StartAppPlugin.java
@@ -5,6 +5,7 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.startapp.sdk.adsbase.StartAppAd;
+import android.util.Log;
 
 @CapacitorPlugin(name = "StartApp")
 public class StartAppPlugin extends Plugin {
@@ -13,20 +14,35 @@ public class StartAppPlugin extends Plugin {
     
     @Override
     public void load() {
-        startAppAd = new StartAppAd(getActivity());
+        Log.d("StartAppPlugin", "=== LOAD METHOD CALLED ===");
+        try {
+            if (getActivity() != null) {
+                startAppAd = new StartAppAd(getActivity());
+                Log.d("StartAppPlugin", "StartAppAd created successfully");
+            } else {
+                Log.e("StartAppPlugin", "Activity is null in load()");
+            }
+        } catch (Exception e) {
+            Log.e("StartAppPlugin", "Error in load(): " + e.getMessage());
+            e.printStackTrace();
+        }
     }
     
-  @PluginMethod
-public void showInterstitial(PluginCall call) {
-    try {
-        if (startAppAd != null) { 
-            startAppAd.showAd();
-            call.resolve();
-        } else {
-            call.reject("StartApp ad not initialized");
+    @PluginMethod
+    public void showInterstitial(PluginCall call) {
+        Log.d("StartAppPlugin", "=== showInterstitial METHOD CALLED ===");
+        try {
+            if (startAppAd != null) { 
+                Log.d("StartAppPlugin", "Showing ad...");
+                startAppAd.showAd();
+                call.resolve();
+            } else {
+                Log.e("StartAppPlugin", "startAppAd is null");
+                call.reject("StartApp ad not initialized");
+            }
+        } catch (Exception e) {
+            Log.e("StartAppPlugin", "Error showing ad: " + e.getMessage());
+            call.reject("Error showing interstitial: " + e.getMessage());
         }
-    } catch (Exception e) {
-        call.reject("Error showing interstitial: " + e.getMessage());
     }
-}
 }
