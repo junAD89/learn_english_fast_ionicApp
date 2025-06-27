d75fda909b012426c4d9c4bd9a519f8f5f51a8c6
junAD89 <balhighlight@gmail.com>
2025-06-27 16:53:48 +0000

    add code in main activity

--- android/app/src/main/java/com/studiesapp/nom/MainActivity.java
+++ android/app/src/main/java/com/studiesapp/nom/MainActivity.java
@@ -1,5 +1,16 @@
 package com.studiesapp.nom;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;  
+import com.startapp.sdk.adsbase.StartAppSDK;  
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+    @Override 
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        // init start io sdk
+        StartAppSDK.init(this, "205126138", false);
+    }
+}
